69b13942427241f14831a14cc940b617ea6b8dd0
FezMLG <46359181+FezMLG@users.noreply.github.com>
2022-03-06 01:27:02 +0100

    deleting unused consts

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,2 @@
-export const WEATHERBIT_API_KEY = 'WEATHERBIT_API_KEY';
-export const OPEN_WEATHER_API_KEY = 'OPEN_WEATHER_API_KEY';
-export const WEATHERBIT = 'WEATHERBIT';
-export const OPEN_WEATHER = 'OPEN_WEATHER';
-export const PROVIDER_NOT_RECOGNIZED =
-  'Weather API provider was not recognized';
+export const WEATHERBIT = "WEATHERBIT";
+export const OPEN_WEATHER = "OPEN_WEATHER";
